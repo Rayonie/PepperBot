f5e9bfb43cc60861ecbef96053d2e5868cc9a597
Rayonie <21045028@myrp.edu.sg>
2023-06-28 21:08:15 +0800

    Tic Tac Toe Activity done, with little of chat function of pepper

--- app/src/main/java/com/example/my4throbotapplication/GameActivity.java
+++ app/src/main/java/com/example/my4throbotapplication/GameActivity.java
@@ -2,13 +2,158 @@ package com.example.my4throbotapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    boolean playerOneActive;
+    private TextView playerOneScore, playerTwoScore, playerStatus;
+    private Button[] buttons = new Button[9];
+    private Button reset, playagain;
+
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int rounds;
+
+    private int playerOneScoreCount, playerTwoScoreCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        playerOneScore = findViewById(R.id.score_Player1);
+        playerTwoScore = findViewById(R.id.score_Player2);
+        playerStatus = findViewById(R.id.textStatus);
+        reset = findViewById(R.id.btn_reset);
+        playagain = findViewById(R.id.btn_play_again);
+
+
+        buttons[0] = findViewById(R.id.button0);
+        buttons[1] = findViewById(R.id.button1);
+        buttons[2] = findViewById(R.id.button2);
+        buttons[3] = findViewById(R.id.button3);
+        buttons[4] = findViewById(R.id.button4);
+        buttons[5] = findViewById(R.id.button5);
+        buttons[6] = findViewById(R.id.button6);
+        buttons[7] = findViewById(R.id.button7);
+        buttons[8] = findViewById(R.id.button8);
+
+        for (int i=0;i< buttons.length;i++)
+        {
+            buttons[i].setOnClickListener(this);
+        }
+
+        playerOneScoreCount = 0;
+        playerTwoScoreCount = 0;
+        playerOneActive = true;
+        rounds = 0;
+
+    }
+
+
+
+    @Override
+    public void onClick(View v)
+    {
+        if(!((Button)v).getText().toString().equals(""))
+        {
+            return;
+        }
+        else if(checkWinner())
+        {
+            return;
+        }
+        String buttonID = v.getResources().getResourceEntryName(v.getId());
+        int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length()-1,buttonID.length()));
+
+        if(playerOneActive)
+        {
+            ((Button)v).setText("X");
+            ((Button)v).setTextColor(Color.parseColor("#ffc34a"));
+            gameState[gameStatePointer] = 0;
+        }
+        else
+        {
+            ((Button)v).setText("O");
+            ((Button)v).setTextColor(Color.parseColor("#70fc3a"));
+            gameState[gameStatePointer] = 1;
+        }
+        rounds++;
+
+        if(checkWinner())
+        {
+            if(playerOneActive)
+            {
+                playerOneScoreCount++;
+                updatePlayerScore();
+                playerStatus.setText("Player 1 has won");
+            }
+            else
+            {
+                playerTwoScoreCount++;
+                updatePlayerScore();
+                playerStatus.setText("Player 2 has won");
+            }
+        }
+        else if(rounds == 9)
+        {
+            playerStatus.setText("No Winner");
+        }
+        else
+        {
+            playerOneActive = !playerOneActive;
+        }
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAgain();
+                playerOneScoreCount = 0;
+                playerTwoScoreCount = 0;
+                updatePlayerScore();
+            }
+        });
+
+        playagain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAgain();
+            }
+        });
+
+
+    }
+
+    private boolean checkWinner() {
+        boolean winnerResults = false;
+        for(int[] winningPositions:winningPositions){
+            if(gameState[winningPositions[0]] == gameState[winningPositions[1]] && gameState[winningPositions[1]]== gameState[winningPositions[2]] && gameState[winningPositions[0]] != 2 ){
+                winnerResults = true;
+            }
+        }
+
+        return winnerResults;
+    }
+
+    private void playAgain() {
+        rounds = 0;
+        playerOneActive = true;
+        for (int i=0; i <buttons.length;i++){
+            gameState[i] = 2;
+            buttons[i].setText("");
+        }
+        playerStatus.setText("Status");
+    }
+
+    private void updatePlayerScore()
+    {
+        playerOneScore.setText(Integer.toString(playerOneScoreCount));
+        playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
+
     }
 }
--- app/src/main/java/com/example/my4throbotapplication/MainActivity.java
+++ app/src/main/java/com/example/my4throbotapplication/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.my4throbotapplication;
 
 //import com.aldebaran.qi.sdk;
+import static android.content.ContentValues.TAG;
+
+import com.aldebaran.qi.Future;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.aldebaran.qi.sdk.builder.AnimateBuilder;
 import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.builder.ChatBuilder;
+import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
+import com.aldebaran.qi.sdk.builder.TopicBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.object.actuation.Animate;
 import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
 import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.conversation.Topic;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -22,6 +33,7 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
 
 
     ImageButton Coursebtn,Careerbtn, Gamebtn , Internbtn;
+    private Chat chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,11 +112,33 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         // Execute the action.
         //say.run();
         //animate.run();
+
+        //Topic topic = TopicBuilder.with(qiContext)
+        //        .withResource(R.raw.greetings)
+       //         .build();
+
+        //QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
+        //        .withTopic(topic)
+        //        .build();
+
+       // chat = ChatBuilder.with(qiContext)
+        //        .withChatbot(qiChatbot)
+         //       .build();
+        //chat.addOnStartedListener(()-> Log.i(TAG,"Discussion Started."));
+        //Future<Void> chatFuture = chat.async().run();
+        //chatFuture.thenConsume(future -> {
+        //    if (future.hasError()) {
+        //        Log.e(TAG, "Discussion finished with error.", future.getError());
+        //    }
+        //});
     }
 
     @Override
     public void onRobotFocusLost() {
         // The robot focus is lost.
+    //    if(chat!=null){
+     //       chat.removeAllOnStartedListeners();
+     //   }
     }
 
     @Override
