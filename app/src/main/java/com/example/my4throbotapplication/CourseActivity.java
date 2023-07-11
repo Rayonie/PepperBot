78af848e6ed7a5901c49fe527c6dbb05f7353704
bryficious <21012434@myrp.edu.sg>
2023-07-11 15:45:48 +0800

    course

--- app/src/main/java/com/example/my4throbotapplication/CourseActivity.java
+++ app/src/main/java/com/example/my4throbotapplication/CourseActivity.java
@@ -16,16 +16,37 @@ import com.aldebaran.qi.sdk.object.conversation.Say;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.example.my4throbotapplication.databinding.ActivityCourseBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseActivity extends RobotActivity implements RobotLifecycleCallbacks {
 
+    Button Log_out;
+
+    ViewPager2 viewPager2;
+
+    //implementing auto slide facility
+
+    private Handler slideHandler = new Handler();
 
 
     @Override
@@ -33,7 +54,64 @@ public class CourseActivity extends RobotActivity implements RobotLifecycleCallb
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
 
+        viewPager2 = findViewById(R.id.viewPager);
 
+        List<SlideItem> slideItem = new ArrayList<>();
+        slideItem.add(new SlideItem(R.drawable.background));
+        slideItem.add(new SlideItem(R.drawable.slide1));
+        slideItem.add(new SlideItem(R.drawable.slide2));
+        slideItem.add(new SlideItem(R.drawable.slide3));
+
+        viewPager2.setAdapter(new SlideAdapter(slideItem,viewPager2));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(5);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositionTransform = new CompositePageTransformer();
+        compositionTransform.addTransformer(new MarginPageTransformer(30));
+        compositionTransform.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f );
+            }
+        });
+
+        viewPager2.setPageTransformer(compositionTransform);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                slideHandler.removeCallbacks(slideRunnable);
+                slideHandler.postDelayed(slideRunnable, 4000);
+            }
+        });
+
+
+    }
+    private Runnable slideRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        slideHandler.removeCallbacks(slideRunnable);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        slideHandler.postDelayed(slideRunnable, 4000);
     }
 
 
