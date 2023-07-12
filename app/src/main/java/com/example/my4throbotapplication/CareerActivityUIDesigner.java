59882431fb55836a053ca413c87c61d2d1b27351
huii0v0 <21016163@myrp.edu.sg>
2023-07-12 10:14:22 +0800

    career

--- app/src/main/java/com/example/my4throbotapplication/CareerActivityUIDesigner.java
+++ app/src/main/java/com/example/my4throbotapplication/CareerActivityUIDesigner.java
@@ -1,24 +1,49 @@
 package com.example.my4throbotapplication;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.VideoView;
 
-public class CareerActivityUIDesigner extends AppCompatActivity {
+import com.aldebaran.qi.Future;
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.builder.ChatBuilder;
+import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.builder.TopicBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.object.conversation.Topic;
+
+public class CareerActivityUIDesigner extends AppCompatActivity implements RobotLifecycleCallbacks{
+    private RobotActivity robotActivity;
 
     VideoView career_vv1;
     ImageView career_ui, career_ux, career_develop, career_analyst;
+    private Chat chat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_uidesigner);
+        QiSDK.register(this, this);
 
         career_vv1 = findViewById(R.id.careervideo1);
         career_ui = findViewById(R.id.planet3);
@@ -87,9 +112,66 @@ public class CareerActivityUIDesigner extends AppCompatActivity {
         super.onPause();
     }
 
+    //@Override
+//    protected void onDestroy() {
+//        career_vv1.stopPlayback();
+//        super.onDestroy();
+//    }
+
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        robotActivity.onDestroy();
+//    }
+
     @Override
-    protected void onDestroy() {
-        career_vv1.stopPlayback();
-        super.onDestroy();
+    public void onRobotFocusGained(QiContext qiContext) {
+
+        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("vrooooom! We have arrived at the career opportunities!") // Set the text to say.
+                .build(); // Build the say action.// Create a new say action.
+
+        Animation myAnimation = AnimationBuilder.with(qiContext)
+                .withResources(R.raw.raise_left_hand_b004)
+                .build();
+        Animate animate = AnimateBuilder.with(qiContext)
+                .withAnimation(myAnimation)
+                .build();
+
+        // Execute the action.
+        say.run();
+        animate.run();
+
+//        Topic topic = TopicBuilder.with(qiContext)
+//                .withResource(R.raw.chat_about_school)
+//                .build();
+//
+//        QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
+//                .withTopic(topic)
+//                .build();
+//
+//        chat = ChatBuilder.with(qiContext)
+//                .withChatbot(qiChatbot)
+//                .build();
+//        chat.addOnStartedListener(()-> Log.i(TAG,"Discussion Started."));
+//        Future<Void> chatFuture = chat.async().run();
+//
+//        chatFuture.thenConsume(future -> {
+//            if (future.hasError()) {
+//                Log.e(TAG, "Discussion finished with error.", future.getError());
+//            }
+//        });
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // The robot focus is lost.
+//        if(chat!=null){
+//            chat.removeAllOnStartedListeners();
+//        }
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
     }
 }
