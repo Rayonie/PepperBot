95995a33e38412e6ca6ec933fcd54221b42071f6
huii0v0 <21016163@myrp.edu.sg>
2023-07-12 10:33:12 +0800

    career

--- app/src/main/java/com/example/my4throbotapplication/CareerActivityDeveloper.java
+++ app/src/main/java/com/example/my4throbotapplication/CareerActivityDeveloper.java
@@ -10,6 +10,15 @@ import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
 public class CareerActivityDeveloper extends CareerActivityUIDesigner {
 
     ImageView career_ui, career_ux, career_develop, career_analyst;
@@ -18,6 +27,7 @@ public class CareerActivityDeveloper extends CareerActivityUIDesigner {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_developer);
+        QiSDK.register(this, this);
 
         career_ui = findViewById(R.id.planet3);
         career_ux = findViewById(R.id.planet4);
@@ -76,5 +86,38 @@ public class CareerActivityDeveloper extends CareerActivityUIDesigner {
     protected void onDestroy() {
         career_vv1.stopPlayback();
         super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("siiiiuuuuuuuuuuuuuuuuu! Here, we can learn about being a Mobile and Web Developer") // Set the text to say.
+                .build(); // Build the say action.// Create a new say action.
+
+        Animation myAnimation = AnimationBuilder.with(qiContext)
+                .withResources(R.raw.raise_left_hand_b004)
+                .build();
+        Animate animate = AnimateBuilder.with(qiContext)
+                .withAnimation(myAnimation)
+                .build();
+
+        // Execute the action.
+        say.run();
+        animate.run();
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // The robot focus is lost.
+//        if(chat!=null){
+//            chat.removeAllOnStartedListeners();
+//        }
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
     }
 }
